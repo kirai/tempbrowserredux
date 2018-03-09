950c016b266f6fa0c6f11b34c619657034e37792
kirai <hectorgarcia@gmail.com>
2018-03-09 15:56:15 +0900

    Chapuza para bindear this

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,21 +4,22 @@ export default class SearchBar extends Component {
     constructor(props){
         super(props);
         this.state = { term: ''};
+        this.onInputChange = this.onInputChange.bind(this);
     }
 
     onInputChange(event) {
         console.log(event.target.value);
+        this.setState({ term: event.target.value});
     }
 
     render() {
         return (
             <form className="input-group">
                 <input 
-                    placeHolder="Get five day forecast"
+                    placeholder="Get five day forecast"
                     className="form-control"
                     value={this.state.term}
-                    onChange={this.onInputChange}
-                />
+                    onChange={this.onInputChange} />
 
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
