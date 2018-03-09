5c1ea8dcc3e9dde4ebbfaf8cda540627ac5de9bc
kirai <hectorgarcia@gmail.com>
2018-03-09 17:07:57 +0900

    console logging the shit out of everything

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props){
         super(props);
         this.state = { term: ''};
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFromSubmit = this.onFromSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -17,7 +21,8 @@ export default class SearchBar extends Component {
 
         // Fetch the weather data
         // api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
-        
+        this.props.fetchWeather(this.state.term);
+        this.setState({term : ''})
     }
 
     render() {
@@ -36,3 +41,9 @@ export default class SearchBar extends Component {
         )
     }
 }
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,4 @@
+export default function(state = null, action) {
+    console.log('baba:', action);
+    return state;
+}
