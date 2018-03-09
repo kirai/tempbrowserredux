116cef1df84fd8cc5923b3cbafb07a581d88b64c
kirai <hectorgarcia@gmail.com>
2018-03-09 15:50:16 +0900

    input placeholder

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-
 import SearchBar from '../containers/search_bar';
-
 
 export default class App extends Component {
   render() {
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = { term: ''};
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input 
+                    placeHolder="Get five day forecast"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
             </form>
         )
     }
